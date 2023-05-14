508741e95e146d2b3ff3d600ee7410a3a0c2b753
LeoYeudiel <lyeuprsw@gmail.com>
2023-05-14 11:03:33 -0600

    add modal for confirm contratation

---
+++ js/script_contratacion.js
@@ -0,0 +1,19 @@
+const confirmar = () => {
+  document.getElementById('name-content').textContent = document.getElementById('name').value
+  document.getElementById('name-content2').textContent = document.getElementById('name').value
+  document.getElementById('app-content').textContent = document.getElementById('app').value
+  document.getElementById('apm-content').textContent = document.getElementById('apm').value
+  document.getElementById('tel-content').textContent = document.getElementById('tel').value
+  document.getElementById('email-content').textContent = document.getElementById('email').value
+  document.getElementById('calle-content').textContent = document.getElementById('calle').value
+  document.getElementById('colonia-content').textContent = document.getElementById('colonia').value
+  document.getElementById('entidad-content').textContent = document.getElementById('entidad').value
+  document.getElementById('alcaldia-content').textContent = document.getElementById('alcaldia').options[document.getElementById('alcaldia').selectedIndex].textContent
+  document.getElementById('rfc-content').textContent = document.getElementById('rfc').value
+  document.getElementById('type_evento-content').textContent = document.getElementById('evento').options[document.getElementById('evento').selectedIndex].textContent
+  document.getElementById('other_evento-content').textContent = document.getElementById('otro_evento').value
+  document.getElementById('people-content').textContent = document.getElementById('people').value
+  document.getElementById('dj-content').textContent = document.getElementById('dj').options[document.getElementById('dj').selectedIndex].textContent
+  document.getElementById('salon-content').textContent = document.getElementById('salon').options[document.getElementById('salon').selectedIndex].textContent
+  document.getElementById('costo-content').textContent = document.getElementById('cost').value
+}
